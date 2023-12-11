57330f8b49a0dabc4bb1a18aaa1c9a8fb63c8154
Tahirid1812210 <cs1812210@szabist.pk>
2023-12-11 11:36:56 +0300

    Navbar Created

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image } from '@chakra-ui/react';
+import logo from '../assets/logo.webp';
+
+const Navbar = () => {
+  return (
+    <HStack justifyContent={'space-between'} padding="10px">
+      <Image src={logo} boxSize="60px" />
+    </HStack>
+  );
+};
+
+export default Navbar;
